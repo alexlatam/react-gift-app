afb88a6c876f3f8823d22a3a886e756ddc926914
Alexis Montilla <alexisamm9261@gmail.com>
2023-04-20 09:17:51 -0400

    feature: build created

--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -1,6 +1,6 @@
 export const getGifts = async (category) => {
     const api_key = 'cuXhvW1uWVQmEe8KTMGY5FR6TdMICpKR'
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=15`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}`
     const resp = await fetch(url);
     const { data } = await resp.json();
 
